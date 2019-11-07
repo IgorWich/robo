56dd15ab9f31ce469927b3c4d03a90e2ddb434b7
igorwi <i.wicherek@hotmail.com>
2019-11-07 19:18:47 +0100

    Updates

---
+++ precache-manifest.ac4072f3ffbddc7d57d5c95fdd8c4bb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e6a1016b14917145959dce870c9876",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7b8a5d5645ed909df9c3",
+    "url": "/robo/static/css/main.c5d47574.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robo/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "7b8a5d5645ed909df9c3",
+    "url": "/robo/static/js/main.2fbf981e.chunk.js"
+  },
+  {
+    "revision": "c6914c600661a516fd7a",
+    "url": "/robo/static/js/runtime-main.ba6cb58a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
